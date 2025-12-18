851db8abbfed918a0dc1743e022882974ec10deb
Ammuu05 <subashinig32@gmail.com>
2025-12-18 21:45:45 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const { signup } = require("./controllers/controls");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.post("/api/signup", signup);
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
