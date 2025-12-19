6fbca289e02afe7e5cf0d8e2951741f2c0c7c948
Ammuu05 <subashinig32@gmail.com>
2025-12-19 17:09:48 +0530

    Modified

--- db.js
+++ db.js
@@ -1,13 +1,7 @@
 const Loki = require("lokijs");
 const path = require("path");
 const dbPath = path.join("C:\\Users\\Welcome\\Desktop\\SignUpPage\\MedicalImage-Diagnosis\\Database", "users.json");
-
-const db = new Loki(dbPath, {
-  autoload: true,
-  autosave: true,
-  autosaveInterval: 4000,
-  autoloadCallback: databaseInitialize,
-});
+const db = new Loki(dbPath, { autoload: true, autosave: true, autosaveInterval: 4000, autoloadCallback: databaseInitialize});
 
 let users;
 function databaseInitialize() {
