b53e6924a7836c2485cf91465f435b2ff9a16071
Ammuu05 <subashinig32@gmail.com>
2025-12-19 10:02:05 +0530

    Initial commit

---
+++ db.js
@@ -0,0 +1,23 @@
+const Loki = require("lokijs");
+const path = require("path");
+const dbPath = path.join("C:\\Users\\Welcome\\Desktop\\Signup_API\\Database", "users.json");
+
+const db = new Loki(dbPath, {
+  autoload: true,
+  autosave: true,
+  autosaveInterval: 4000,
+  autoloadCallback: databaseInitialize,
+});
+
+let users;
+function databaseInitialize() {
+  users = db.getCollection("users");
+  if (!users) {
+    users = db.addCollection("users", {
+      unique: ["email"],
+    });
+  }
+  db.saveDatabase();
+}
+
+module.exports = () => users;
