8f48ee63767ea2dd7cfef095c7c1c0fb3dc5c1e4
Ammuu05 <subashinig32@gmail.com>
2025-12-19 17:04:45 +0530

    Modified

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const { signup } = require("./controllers/controls");
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 app.post("/signup", signup);
