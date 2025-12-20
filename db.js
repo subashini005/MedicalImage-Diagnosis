86d0475cbbde01d19759443c98726d41d3ca65f7
Ammuu05 <subashinig32@gmail.com>
2025-12-20 10:33:36 +0530

    Modified

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const Loki = require("lokijs");
-const path = require("path");
-const dbPath = path.join(__dirname, "Database", "users.json");
+const dbPath = __dirname + "/Database/users.json";
 const db = new Loki(dbPath, { autoload: true, autosave: true, autosaveInterval: 4000, autoloadCallback: databaseInitialize});
 
 let users;
